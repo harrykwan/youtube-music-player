22d9f6fd2810cd338854d5aa1fba75038df48635
harrykwan <harry1998kwan@gmail.com>
2023-02-07 18:18:42 +0800

    start

---
+++ index.js
@@ -0,0 +1,31 @@
+const stream = require("youtube-audio-stream");
+const youtubesearchapi = require("youtube-search-api");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/search/:keywords", async (req, res) => {
+  const keywords = req.params.keywords;
+  const result = await youtubesearchapi.GetListByKeyword(keywords);
+  res.send(result);
+});
+
+app.get("/audio/:videoId", async (req, res) => {
+  for await (const chunk of stream(
+    `http://youtube.com/watch?v=${req.params.videoId}`
+  )) {
+    res.write(chunk);
+  }
+  res.end();
+});
+
+app.use(express.static("frontend"));
+
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App is listening at http://localhost:${port}`);
+});
